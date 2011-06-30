ce4379c9cbc74d809ba0942fbcf754665e35bb17
bigwheel <k.bigwheel+tech@gmail.com>
2011-06-30 11:17:18 +0900

    いろいろあって、セッション管理を断念した。理由としては、一度アクセストークンを取得した後にもう一度OAuth認証した時、昔のアクセストークンが向こうとなってしまう可能性があり、セッション管理だともう一度設定しようとした時、過去のpost予約が無効になってしまう可能性があるため。 ログイン式ならアカウントと紐付すればいいので、再度OAuth認証する必要はない。

--- src/appengine_config.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from gaesessions import SessionMiddleware
-
-def webapp_add_wsgi_middleware(app):
-    app = SessionMiddleware(app, cookie_key="8xGsf6jTyRjzzaBPL5EF33KWYiB5NkphJk7KxPMgdrfpf4cxsT")
-    return app
--- src/cleanup_sessions.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from gaesessions import delete_expired_sessions
-while not delete_expired_sessions():
-    pass
-
-print "expired_sessions are deleted."
