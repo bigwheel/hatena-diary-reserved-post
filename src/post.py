d7b722508c71cbdef9ddc5599359cb8654fcc10a
bigwheel <k.bigwheel+tech@gmail.com>
2011-11-28 14:26:43 +0900

    ログを取る環境の構築と、例外時の処理を設定した。

--- src/post.py
+++ src/post.py
@@ -3,6 +3,7 @@
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 import datetime
+import logging
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -13,16 +14,21 @@ from main import ReservedPost, UserProperty
 
 class MainPage(webapp.RequestHandler):
     def get(self, mode=""):
-        hatenaOauthClient = oauth.HatenaClient("bLgBYjM0mxzK7Q==", "BUDLSz9wMZgjJgxiFihI5uogoPQ=",
-                                               None, scope = "read_public,write_public," +
-                                               "read_private,write_private")
+        try:
+            hatenaOauthClient = oauth.HatenaClient("bLgBYjM0mxzK7Q==", "BUDLSz9wMZgjJgxiFihI5uogoPQ=",
+                                                   None, scope = "read_public,write_public," +
+                                                   "read_private,write_private")
+        except:
+            logging.critical("はてなOauthのアプリケーション開発者とはてな間の認証が通りません。" +
+                             "おそらくこのアプリケーション用のキーおよびトークン" +
+                             "(上のわけのわからない2つの文字列)が切れたか、はてな側の不具合と思われます。")
+            return
 
-        pastTask = ReservedPost.gql("WHERE date <= DATETIME('%s')" % 
+        queuedTask = ReservedPost.gql("WHERE date <= DATETIME('%s')" % 
                              (datetime.datetime.now() + datetime.timedelta(hours=9))
                              .strftime(u"%Y-%m-%d %H:%M:%S"))
-        for reservedPost in pastTask:
-            userProperty = UserProperty.gql("WHERE g_username = :1",
-                                            reservedPost.g_username).get()
+        for reservedPost in queuedTask:
+            userProperty = UserProperty.gql("WHERE g_username = :1", reservedPost.g_username).get()
             result = hatenaOauthClient.make_request(url=reservedPost.url, token=userProperty.accessToken,
                                          secret=userProperty.accessSecret, method=urlfetch.PUT,
                                          headers={"X-HATENA-PUBLISH":"1"})
