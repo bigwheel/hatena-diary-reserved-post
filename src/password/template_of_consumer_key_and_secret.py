6066f909514dea181a0a083bd829cc99e4698b73
bigwheel <k.bigwheel+tech@gmail.com>
2012-01-16 20:17:23 +0900

    consumer_keyおよびconsumer_secretをバージョン管理から外し、 そのモックをバージョン管理に追加した。

---
+++ src/password/template_of_consumer_key_and_secret.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
+# このファイルはconsumer_key_and_secret.pyのテンプレートです。
+# 実際のサービスはこのファイルをconsumer_key_and_secret.pyにリネームし、
+# 下の関数の返り値にそれぞれconsumer_keyおよびconsumer_secretを入力して
+# 実行されます
+
+def getConsumerKey():
+    return "こんしゅーまー・きー"
+
+def getConsumerSecret():
+    return "こんしゅーまー・しーくれっと"
